c2d140398ff47e2e308b6bca5f610be1985e4202
pgt112you <pgt112you@qq.com>
2018-03-28 23:23:51 +0800

    aaa

--- ptr_test.cpp
+++ ptr_test.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void func() {
+void func() {    // 这个最后两个输出都是35
     //shared_ptr<int> pi(new int{5});    // 这一行和下面一行都可以
     shared_ptr<int> pi(new int(5));
     *pi = 6;
@@ -16,7 +16,7 @@ void func() {
     cout << *pi1 << endl;
 }
 
-void func1() {
+void func1() {   // 这个会挂掉
     auto_ptr<int> pi(new int(5));
     *pi = 6;
     auto_ptr<int> pi1 = pi;
@@ -26,9 +26,19 @@ void func1() {
     cout << *pi1 << endl;
 }
 
+void func2() {
+    unique_ptr<int> pi(new int(5));
+    unique_ptr<int> pi1(new int(15));
+    pi.reset(pi1.release());
+    pi1.reset(new int{25});
+    cout << *pi << endl;
+    cout << *pi1 << endl;
+}
+
 int main() {
     func();
     sleep(1);
-    func1();
+    //func1();
     sleep(1);
+    func2();
 }
